094fe33c785c14b40a43ecc798b9c2ad22e8e283
Arunkumar Narayanaswamy <arunkmr1983@gmail.com>
2022-08-20 11:12:21 +0100

    using istio gateway

--- istio_demo/install_instructions.sh
+++ istio_demo/install_instructions.sh
@@ -1,3 +1,5 @@
 #create a configmap to store protofiles
 kubectl delete configmap myshop.proto 
 kubectl create configmap myshop.proto --from-file="./protos/myshop.proto" 
+
+kubectl apply -f .\k8s\
